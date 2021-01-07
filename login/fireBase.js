1ed5a4a9fbeeaa44d241b323421e8cbe50b78830
gpt18 <gudduprajapati626@gmail.com>
2021-01-07 13:42:21 +0530

    added subjects in sem 3

---
+++ login/fireBase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDfuUTWG1rSRXu8gJer-sMJH7iBwkEk758",
+    authDomain: "examguru-e08aa.firebaseapp.com",
+    projectId: "examguru-e08aa",
+    storageBucket: "examguru-e08aa.appspot.com",
+    messagingSenderId: "343544014920",
+    appId: "1:343544014920:web:a1d2d9c59d9a5eb1e7107d",
+    measurementId: "G-0REJS9PDGQ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
